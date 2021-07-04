521d2247b04d5c2f88826ea4e83fa62a02222aed
mahmoodsa <mahmoodsagharjooghi@gmail.com>
2021-07-04 07:18:59 +0430

    add redux and react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store/reducers/index';
+
+import 'react-toastify/dist/ReactToastify.css';
+import './styles/global.scss';
+
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
