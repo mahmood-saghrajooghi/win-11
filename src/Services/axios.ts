369001f25a7efd82662d634823477a973fef27ad
mahmoodsa <mahmoodsagharjooghi@gmail.com>
2021-07-04 08:23:12 +0430

    add baseurl to axiso

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -1,10 +1,8 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
+const instance: AxiosInstance = axios.create({
+  baseURL: 'https://api.storylia.com/'
+});
 
-const instance = axios.create({
-  headers:{
-    'x-cart-id': localStorage.getItem('cart-id')
-  }
-  // baseURL: 'https://localhost:3005'
-})
+
 
 export default instance;
